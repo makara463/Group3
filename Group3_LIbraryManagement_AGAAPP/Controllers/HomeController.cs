6a8b747f1226e46bb88559b251efa2b8e4c2cbe3
makar <makar@PANDA>
2025-01-09 19:38:05 +0700

    Add project files.

---
+++ Group3_LIbraryManagement_AGAAPP/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Group3_LIbraryManagement_AGAAPP.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Group3_LIbraryManagement_AGAAPP.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Group3_LIbraryManagement_AGAAPP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Group3_LIbraryManagement_AGAAPP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
